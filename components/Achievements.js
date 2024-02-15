32d2ceee63b3455be7fd202b4fff411445094518
Andre Westerlund <andrewest_04@hotmail.com>
2024-02-15 14:37:45 +1300

    add dark mode facelift

--- components/Achievements.js
+++ components/Achievements.js
@@ -40,15 +40,15 @@ const AchievementCard = ({ title, month, year, organization, imgUrl }) => {
             height={800}
             className="object-fill w-full h-full transform duration-700 backdrop-opacity-100"
             />
-            <div className="absolute bg-gradient-to-t from-black w-full h-full flex items-end justify-center -inset-y-0"><h1 className="font-bold text-lg text-white dark:text-gray-300 mb-2 pl-3 pr-1.5">{title}</h1></div>
+            <div className="absolute bg-gradient-to-t from-black w-full h-full flex items-end justify-center -inset-y-0"><h1 className="font-bold text-lg text-white shadow-black dark:text-gray-300 mb-2 pl-3 pr-1.5">{title}</h1></div>
         </div>
         <div className="bg-white dark:bg-slate-800">
             <div className="text-center px-3 pb-6 pt-2">
-                <p className="mt-2 font-sans font-light text-slate-700 dark:text-gray-300">
+                <p className="mt-2 font-sans font-light text-slate-700 dark:text-gray-300 dark:font-bold">
                     {organization}
                 </p>
             </div>
-            <div className="flex justify-center pb-3 text-slate-700 dark:text-gray-400">
+            <div className="flex justify-center pb-3 text-slate-700 dark:text-gray-300">
                 <div className="text-center mr-3 border-r pr-3 last:border-r-0">
                     <h2>{month}</h2>
                 </div>
--- components/Experience.js
+++ components/Experience.js
@@ -41,11 +41,11 @@ export default function Experience() {
 const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
   return (
     <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
-      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
+      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-50">
         {year}
       </h1>
       <h1 className="font-semibold text-xl">{title}</h1>
-      <a href={companyLink} className="text-gray-500">
+      <a href={companyLink} className="text-gray-500 dark:text-gray-100">
         {company}
       </a>
       <p className="text-gray-600 dark:text-gray-400 my-2">{desc}</p>
